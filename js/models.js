61deab9d20993bf3b1c5e5e9bc98a79c0cf1b470
Nick Pope <pope410211@gmail.com>
2015-05-30 13:17:29 -0400

    updating JS

--- js/models.js
+++ js/models.js
@@ -1,5 +1,13 @@
 (function () {
   window.chess ={
 
+var board = {
+  rank= ["1", "2", "3", "4", "5", "6", "7", "8"],
+  file= ["a", "b", "c", "d", "e", "f", "g", "h"]
+};
+
+var piece = function(rank, file);
+
+
   };
 })(window);
